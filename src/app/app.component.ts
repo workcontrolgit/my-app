9bddf98f1acc0c2ef203d3fddf0f5d2219186951
Fuji Nguyen <fuji.nguyen@workcontrol.com>
2025-07-26 09:04:16 -0400

    add adress to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddressManagerComponent, Address } from './address/address-manager.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddressManagerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild('addressManager') addressManager!: AddressManagerComponent;
   title = 'my-app';
 }
